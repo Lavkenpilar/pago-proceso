b36715b4e83f50bb928d855c305a551eee152bae
Lavken <lavkenpilar@gmail.com>
2024-12-13 20:03:56 -0300

    component(Proceso.jsx)

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Proceso from './components/Proceso'
+
+function App() {
+ 
+  return (
+    <>
+     <Proceso/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Proceso.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+const Proceso = () => {
+  return (
+    <Card style={{ width: '54rem' }}>
+    <Card.Img variant="top" src='/2.svg'  style={{width:'54rem'}}/>
+    </Card>
+  )
+}
+
+export default Proceso
